a2458ff3a9e20446e02a3449a9059283b21fd2d1
murugilinet <murugilinet59@gmail.com>
2023-02-16 23:13:36 +0300

    Intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 /**
- * main - a program that prints the size of various types on the computer it is compiled and run on.
+ * main - A program that prints the size of various types on the computer it is compiled and run on.
  * Return : 0(Success)
  */
 #include <stdio.h>
