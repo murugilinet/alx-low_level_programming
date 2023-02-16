498f303248e15305730b1cd8856370f8705bbb02
murugilinet <murugilinet59@gmail.com>
2023-02-16 21:27:33 +0300

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main -  c program that prints line using the puts fuction
+ *Return: 0 (pass)
+ */
+int main(void);
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
+
