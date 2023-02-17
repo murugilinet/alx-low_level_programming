58d74a54bc12311745dec2a950335b3c73455fd9
murugilinet <murugilinet59@gmail.com>
2023-02-17 23:47:07 +0300

    possible combos

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- priny numbers in ascending order
+ * Return:always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
