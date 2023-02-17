a6f19e109a4f8db69eef2a5cae67ca4ba22fa6ae
murugilinet <murugilinet59@gmail.com>
2023-02-17 21:50:44 +0300

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,12 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 {
-	putchar(ch)
+	putchar(ch);
 }
 	for (ch = 'A'; ch <= 'Z'; ch++)
 {
-	putchar(ch)
+	putchar(ch);
 }
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
