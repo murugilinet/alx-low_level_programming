928cfcdf5a95afb12a3e671039777f7b6d93f0f4
murugilinet <murugilinet59@gmail.com>
2023-02-19 18:56:30 +0300

    possible combos

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,10 +7,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 48; num < 58; num++)
+	for (num = 0; num < 10; num++)
 	{
 		putchar(num);
-		if (num != 57)
+		if (num != 9)
 		{
 			putchar(',');
 			putchar(' ');
