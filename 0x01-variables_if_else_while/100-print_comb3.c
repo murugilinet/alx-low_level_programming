cdbdcfa1e9f938eae3d9379052e3562b184ed867
murugilinet <murugilinet59@gmail.com>
2023-02-19 19:44:25 +0300

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,12 +17,12 @@ int main(void)
 				putchar(b % 10 + '0');
 
 			}
-			if (a != 8 && b != 9)
-			{
+				if (a != 8 && b != 9)
+				{
 				putchar(',');
 				putchar(' ');
+				}
 			}
-		}
 		}
 	putchar('\n');
 	return (0);
