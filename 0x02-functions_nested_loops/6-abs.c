91749eb18ac47af6555a491545c8b269c4af833d
murugilinet <murugilinet59@gmail.com>
2023-02-21 22:53:07 +0300

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - absolute value of int
+ * @num: parameter
+ * Return: always num
+ */
+int _abs(int num)
+{
+	if (a < 0)
+		num = -(num);
+	else if (num >= 0)
+		num = num;
+	return (num);
+}
