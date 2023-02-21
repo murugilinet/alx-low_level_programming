2d86668e1ab24be2b42884f545d0603a698aebe7
murugilinet <murugilinet59@gmail.com>
2023-02-21 21:54:37 +0300

    10 x aplhabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- alphabet ten times
+ * Return: always 0;
+ */
+void print_alphabet_x10(void);
+{
+	int i;
+	char lc;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (lc = 'a'; lc <= 'z'; lc++)
+			_putchar(lc);
+		_putchar('\n');
+	}
+}
+
+
