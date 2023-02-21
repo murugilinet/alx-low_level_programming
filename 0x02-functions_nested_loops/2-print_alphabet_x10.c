b0107f2d185445b6194ac70da0ef720df0aac8fc
murugilinet <murugilinet59@gmail.com>
2023-02-21 22:01:48 +0300

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * print_alphabet_x10- alphabet ten times
  * Return: always 0;
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int i;
 	char lc;
@@ -15,5 +15,3 @@ void print_alphabet_x10(void);
 		_putchar('\n');
 	}
 }
-
-
