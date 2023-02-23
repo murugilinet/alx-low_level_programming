387a04b7ce1f93d73461fd0e2adad79e61f341af
murugilinet <murugilinet59@gmail.com>
2023-02-23 20:42:47 +0300

    numbers spear for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,10 @@
  */
 void print_numbers(void)
 {
-	int i;
+	int i = 0;
 
 	for (i >= 0; i <= 9; i++)
-		putchar(i % 10 + '0');
-	putchar('\n');
+		_putchar(i % 10 + '0');
+	_putchar('\n');
 	return (0);
 }
