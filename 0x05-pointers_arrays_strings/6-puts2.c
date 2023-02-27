bd74972f932778b1ead85b4ee474a00e22b804b9
murugilinet <murugilinet59@gmail.com>
2023-02-27 21:34:42 +0300

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints part of a string
+ * @str: string to be printed
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
