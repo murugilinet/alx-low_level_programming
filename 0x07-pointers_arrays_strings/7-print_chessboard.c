0d8d423dc4be3e5e2f14b76ef670b11e7bafb4a6
murugilinet <murugilinet59@gmail.com>
2023-03-07 00:38:30 +0300

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer
+ * Return : nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int a, b;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 0; b < 8; b++)
+			_putchar(a[a][b]);
+		_putchar('\n');
+	}
+}
