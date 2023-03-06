60250f16f89a6a6431280c2a75a5a07ff496d542
murugilinet <murugilinet59@gmail.com>
2023-03-06 21:40:49 +0300

    strspan

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - gets length of a prefis substring
+ * @s: initial seg
+ * @accept:strng being checked against
+ * Return: always 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int r;
+
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '/0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
