de6ceb58ff36fb6494dfbfe41c412a4a719a439d
murugilinet <murugilinet59@gmail.com>
2023-03-08 12:17:06 +0300

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion(char *s)- prints length of string
+ * @s: string input
+ * Return: always 0
+ */
+int _strlen_recursion(char *s)
+{
+	count = 0;
+
+	if (*s != '\0')
+	{
+		count = count + _strlen_recursion(s + 1);
+	}
+	return (count);
+}
