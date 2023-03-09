629f52cdf8cd8335a43a997c51ff13482339f039
murugilinet <murugilinet59@gmail.com>
2023-03-09 10:50:12 +0300

    task7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -24,11 +24,11 @@ int pali_ndrome(int i, char *p)
 	int length = strlen(p);
 	int middle = length / 2;
 
-	if (i <= middle)
+	if (p[i] != p[length - i - 1])
+		return (0);
+	if (p[i] == p[length - i - 1])
 	{
-		if (p[i] != p[length - i - 1])
-			return (0);
-		if (p[i] == p[length - i - 1])
+		if (i < middle)
 		{
 			pali_ndrome(i + 1, p);
 			return (1);
