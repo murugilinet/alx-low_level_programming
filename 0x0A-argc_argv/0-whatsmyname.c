7bd80447e417f4b5b4a4c645f12b39532530bbd8
murugilinet <murugilinet59@gmail.com>
2023-03-10 13:57:32 +0300

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -9,6 +10,6 @@
 int main(int argc, char *argv[])
 	{
 		while (argc--)
-			printf("%s", *argv++);
+			printf("%s\n", *argv++);
 		return (0);
 	}
