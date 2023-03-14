c0bd4d8dd24d94d06db85cfb136c04e47c07fdf0
murugilinet <murugilinet59@gmail.com>
2023-03-14 12:06:25 +0300

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,11 +17,11 @@ char *str_concat(char *s1, char *s2)
 			  */
 	if (s1 ==  NULL)
 	{
-		return ("");
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		return ("");
+		s2 = "";
 	}
 
 	for (i = 0; s1[i] || s2[i]; i++)
