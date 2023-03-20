55cb49f73e39e92c65cf7502e2c42ff231b039ac
murugilinet <murugilinet59@gmail.com>
2023-03-20 16:54:03 +0300

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog- structure
+ * @name: name of
+ * @age: age of
+ * @owner: owner of
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H*/
