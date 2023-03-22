4f00ac2d252f39b2575651e88c257c990ab3015e
murugilinet <murugilinet59@gmail.com>
2023-03-22 14:44:36 +0300

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,13 +14,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		rtn = cmp(array[i]);
-		if (rtn != 0)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			rtn = cmp(array[i]);
+			if (rtn != 0)
+			{
+				return (i);
+			}
 		}
+		return (-1);
 	}
-	return (-1);
 }
