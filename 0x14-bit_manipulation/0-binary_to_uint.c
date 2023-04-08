7ac5686284529cd38aa2ccac0970136256689593
murugilinet <murugilinet59@gmail.com>
2023-04-08 13:25:12 +0300

    update on task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <math.h>
 /**
  * binary_to_uint- converts vinary to unsigned int
  * @b: pointer to a astring
@@ -9,21 +10,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len;
+	int len, i;
 	unsigned int num = 0, base = 1;
 
 	len = strlen(b);
+
 	if (b == NULL)
 		return (0);
-	while (len)
+	for (i = len - 1; i > -1; i--)
 	{
-		if (b[len - 1] != '0' && b[len - 1] != '1')
-			return (0);
-
-		if (b[len - 1] == '1')
-			num += base;
-		base *= 2;
-		len--;
+	if (b[i] != '0' && b[i] != '1')
+		return (0);
+	if (b[i] == '1')
+		num += base;
+	base *= 2;
 	}
 	return (num);
 }
