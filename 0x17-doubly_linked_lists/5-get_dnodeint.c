3bba2963ec8d3189e383fb2325718200fe8d2224
murugilinet <murugilinet59@gmail.com>
2023-06-15 14:09:48 -0700

    update

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,10 +10,10 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
-
+	
 	if (head == NULL)
 		return (NULL);
-	if (i == 0)
+	if (index == 0)
 		return (head);
 	for (i = 0; i < index; i++)
 	{
